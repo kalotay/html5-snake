45ea0807c2aac6e25ce1f944d45e4aef68b2d1b1
Daniel Kalotay <d.kalotay@gmail.com>
2012-07-21 00:01:11 +0200

    removed module pattern
    not worth the extra complexity yet

--- snake.js
+++ snake.js
@@ -1,46 +1,40 @@
-var Snake = (function() {
-    function createArena() {
-        var arena = document.getElementById("arena"),
-            index = WIDTH * HEIGHT,
-            div;
-            
-        while (index-- > 0) {
-            div = document.createElement("div");
-            arena.appendChild(div);
-        }
-        paintSnake();
+function createArena() {
+    var arena = document.getElementById("arena"),
+        index = WIDTH * HEIGHT,
+        div;
+        
+    while (index-- > 0) {
+        div = document.createElement("div");
+        arena.appendChild(div);
     }
-    function paintSnake() {
-        var tiles = document.getElementById("arena").children;
-        tiles[snake.head].className = "head";
-        snake.tail.forEach(function (tailIndex) {
-            tiles[tailIndex].className = "tail";
-        });
-        if (snake.dead != -1) {
-            tile[snake.dead].className = "";
-        }
+    paintSnake();
+}
+function paintSnake() {
+    var tiles = document.getElementById("arena").children;
+    tiles[snake.head].className = "head";
+    snake.tail.forEach(function (tailIndex) {
+        tiles[tailIndex].className = "tail";
+    });
+    if (snake.dead != -1) {
+        tile[snake.dead].className = "";
     }
-    function makeSnake(width, height, length) {
-        var startingIndex = (height / 2) * width,
-            head = startingIndex + length,
-            tail = [];
+}
+function makeSnake(width, height, length) {
+    var startingIndex = (height / 2) * width,
+        head = startingIndex + length,
+        tail = [];
 
-        while (--length > 0) {
-            tail.push(startingIndex + length)
-        }
-        return {
-            "head": head,
-            "tail": tail,
-            "dead": -1
-        };
+    while (--length > 0) {
+        tail.push(startingIndex + length)
     }
-
-    var WIDTH = 40,
-        HEIGHT = 20,
-        snake = makeSnake(WIDTH, HEIGHT, 5);
-    
     return {
-        "createArena": createArena,
-        "paintSnake": paintSnake
+        "head": head,
+        "tail": tail,
+        "dead": -1
     };
-})();
+}
+
+var WIDTH = 40,
+    HEIGHT = 20,
+    snake = makeSnake(WIDTH, HEIGHT, 5);
+
