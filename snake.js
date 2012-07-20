dabac4ee4081c724d2c5693f86d8ad2bbcd8f7ab
Daniel Kalotay <d.kalotay@gmail.com>
2012-07-21 00:46:58 +0200

    it can move;

--- snake.js
+++ snake.js
@@ -7,18 +7,7 @@ function createArena() {
         div = document.createElement("div");
         arena.appendChild(div);
     }
-    paintSnake();
-}
-
-function paintSnake() {
-    var tiles = document.getElementById("arena").children;
-    tiles[snake.head].className = "head";
-    snake.tail.forEach(function (tailIndex) {
-        tiles[tailIndex].className = "tail";
-    });
-    if (snake.dead != -1) {
-        tile[snake.dead].className = "";
-    }
+    snake.paint();
 }
 
 function Snake(width, height, length) {
@@ -27,11 +16,38 @@ function Snake(width, height, length) {
 
     this.head = startingIndex + length;
     this.dead = -1;
+    this.mover = moveRight;
     while (--length > 0) {
         tail.push(startingIndex + length)
     }
     this.tail = tail;
 }
+
+Snake.prototype.paint = function paintSnake() {
+    var tiles = document.getElementById("arena").children;
+    tiles[this.head].className = "head";
+    this.tail.forEach(function (tailIndex) {
+        tiles[tailIndex].className = "tail";
+    });
+    if (this.dead != -1) {
+        tiles[this.dead].className = "";
+    }
+};
+
+Snake.prototype.move = function moveSnake() {
+    this.dead = this.tail.pop();
+    this.tail.unshift(this.head);
+    this.head = this.mover(this.head);
+};
+
+Snake.prototype.moveAndRepaint = function moveAndRepaintSnake() {
+    this.move();
+    this.paint();
+};
+
+function moveRight(snakeHead) {
+    return (snakeHead + 1) % (WIDTH * HEIGHT);
+};
 
 var WIDTH = 40,
     HEIGHT = 20,
