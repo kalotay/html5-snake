372161732800ee1ef35d71eda8560a83800e3850
Daniel Kalotay <d.kalotay@gmail.com>
2012-07-20 23:58:25 +0200

    more work
    I should really have some more discipline while commiting

--- snake.js
+++ snake.js
@@ -8,11 +8,39 @@ var Snake = (function() {
             div = document.createElement("div");
             arena.appendChild(div);
         }
+        paintSnake();
     }
+    function paintSnake() {
+        var tiles = document.getElementById("arena").children;
+        tiles[snake.head].className = "head";
+        snake.tail.forEach(function (tailIndex) {
+            tiles[tailIndex].className = "tail";
+        });
+        if (snake.dead != -1) {
+            tile[snake.dead].className = "";
+        }
+    }
+    function makeSnake(width, height, length) {
+        var startingIndex = (height / 2) * width,
+            head = startingIndex + length,
+            tail = [];
+
+        while (--length > 0) {
+            tail.push(startingIndex + length)
+        }
+        return {
+            "head": head,
+            "tail": tail,
+            "dead": -1
+        };
+    }
+
     var WIDTH = 40,
-        HEIGHT = 20;
+        HEIGHT = 20,
+        snake = makeSnake(WIDTH, HEIGHT, 5);
     
     return {
-        "createArena": createArena
+        "createArena": createArena,
+        "paintSnake": paintSnake
     };
 })();
