8eef991fac8177ca75c0bdbefad0282c01222c6b
Daniel Kalotay <d.kalotay@gmail.com>
2012-07-20 23:19:30 +0200

    arena generated

--- snake.js
+++ snake.js
@@ -1,8 +1,18 @@
 var Snake = (function() {
     function createArena() {
+        var arena = document.getElementById("arena"),
+            index = WIDTH * HEIGHT,
+            div;
+            
+        while (index-- > 0) {
+            div = document.createElement("div");
+            arena.appendChild(div);
+        }
     }
-
+    var WIDTH = 40,
+        HEIGHT = 20;
+    
     return {
-        "createArena": createArena;
+        "createArena": createArena
     };
-})()
+})();
